a5b396de2efc71db5d58b903da10f9db6e696150
fateme.dev <155579918+fatemeMohamadian@users.noreply.github.com>
2024-06-15 00:21:00 +0330

    Used JavaScript

---
+++ js/master.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 3,
+    speed: 500,
+    loop: true,
+    rotate: true,
+    mousewheel: {
+        invert: true,
+    },
+    // autoplay:{
+    //     delay:2000,
+    // },
+});
